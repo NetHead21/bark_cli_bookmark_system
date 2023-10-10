aad556a1a0ca9df4991778d18fb94e196fdd9e64
Juniven <juniven.s@egrowthpartners.com>
2023-10-10 23:23:09 +0800

    Adding Menu Options

---
+++ main.py
@@ -0,0 +1,42 @@
+import commands
+from collections import OrderedDict
+
+
+class Option:
+    def __init__(self, name: str, command: object, prep_call: str = None) -> None:
+        self.name = name
+        self.command = command
+        self.prep_call = prep_call
+
+    def choose(self):
+        data = self.prep_call() if self.prep_call else None
+        message = self.command.execute(data) if data else self.command.execute()
+        print(message)
+
+    def __str__(self):
+        return self.name
+
+
+def print_options(options: dict) -> None:
+    for shortcut, option in options.items():
+        print(f"({shortcut}) {option}")
+    print()
+
+
+if __name__ == "__main__":
+    print("Welcome to Bark!")
+    options = OrderedDict(
+        {
+            "A": Option("Add a bookmark", commands.AddBookmarkCommand()),
+            "B": Option("List bookmarks by date", commands.ListBookmarksCommand()),
+            "T": Option(
+                "List bookmarks by title",
+                commands.ListBookmarksCommand(order_by="title"),
+            ),
+            "D": Option("Delete a bookmark", commands.DeleteBookmarkCommand()),
+            "Q": Option("Quit", commands.QuitCommand()),
+        }
+    )
+    print_options(options)
+
+    commands.CreateBookmarksTableCommand().execute()
